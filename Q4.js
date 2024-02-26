afb2186cca8cfd3def5daaa3fd125b453154c81b
Aliza Moin <160245045+aliza-moin18@users.noreply.github.com>
2024-02-26 23:00:51 +0500

    Add files via upload

---
+++ Q4.js
@@ -0,0 +1,4 @@
+// 4. Famous Quotes: Find a quotefrom a famous person you admire. Print the quotr and the name of its author . Your output should look something like the follwing , including the quotation marks:
+// Albert Einstein once said, "A person who never made a mistake never tried anything new."
+var name1 = ("John Lennon said: ");
+console.log("".concat(name1, "\"Reality leaves a lot to the imagination.\""));
---
+++ Q4.ts
@@ -0,0 +1,7 @@
+// 4. Famous Quotes: Find a quotefrom a famous person you admire. Print the quotr and the name of its author . Your output should look something like the follwing , including the quotation marks:
+// Albert Einstein once said, "A person who never made a mistake never tried anything new."
+
+
+let name1: string = ("John Lennon said: ")
+
+console.log(`${name1}"Reality leaves a lot to the imagination." `)
